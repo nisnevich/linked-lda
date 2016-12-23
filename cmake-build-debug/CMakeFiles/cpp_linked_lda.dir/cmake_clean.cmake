efa56a340033444bb80453253758263c7f3969e7
nisnevich <officialsagorbox@gmail.com>
2016-12-23 11:03:05 +0300

    Added configuration files for distributed computing (attempt №3)

---
+++ cmake-build-debug/CMakeFiles/cpp_linked_lda.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_linked_lda.dir/src/ConfigFile.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/Corpus.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/CountTables.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/Document.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/linkedLDA.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/LinkGraph.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/Model.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/ModelProperties.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/MultinomialDistribution.cpp.o"
+  "CMakeFiles/cpp_linked_lda.dir/src/Vocabulary.cpp.o"
+  "cpp_linked_lda.pdb"
+  "cpp_linked_lda"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_linked_lda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
